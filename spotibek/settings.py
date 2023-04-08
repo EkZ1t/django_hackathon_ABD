ca60146934bf317d0385a8616237365fe7bde435
EkZ1t <mr.koteika041@gmail.com>
2023-04-08 13:36:22 +0600

    forgot to hide secret key

--- spotibek/settings.py
+++ spotibek/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2vuzecx()iqssr#wf7+4b)g42=it&iuzx$8$^*ehnrpwbu)7uj'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
